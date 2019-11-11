2431cd3627bc369e5a0c65c33ad1f4d9aa131e77
Vincent Siauw <jamesvincentsiauw@gmail.com>
2019-11-11 14:04:39 +0700

    update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var studentRouter = require('./routes/student');
