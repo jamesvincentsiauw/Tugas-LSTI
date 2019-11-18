f5bd62817a3ef1c4b1e8e7cfdf6119a420bc2472
Vincent Siauw <jamesvincentsiauw@gmail.com>
2019-11-18 17:28:00 +0700

    CORS Enabled

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ var auth = function(req,res,next) {
     }
 };
 var app = express();
+var cors = require('cors');
 const session = require('express-session');
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
